56b041d6b62137074e5a14600689d18122b19f9c
Luiz <lufelipe111@gmail.com>
2020-12-12 15:28:38 -0300

    Inclusão de SemanticException

---
+++ src/br/com/professorisidro/isilanguage/exceptions/IsiSemanticException.java
@@ -0,0 +1,7 @@
+package br.com.professorisidro.isilanguage.exceptions;
+
+public class IsiSemanticException extends RuntimeException{
+    public IsiSemanticException(String msg) {
+        super(msg);
+    }
+}
